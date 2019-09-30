fec343e5b948ae190f058fcac91114ff7473ebc9
zd-zero <zaerald.zd@gmail.com>
2019-09-30 16:12:50 +0800

    feat: show notif and play sound on startup

---
+++ src/main/java/zd/zero/waifu/motivator/plugin/WaifuMotivator.java
@@ -0,0 +1,58 @@
+package zd.zero.waifu.motivator.plugin;
+
+import com.intellij.notification.Notification;
+import com.intellij.notification.NotificationType;
+import com.intellij.notification.Notifications;
+import com.intellij.openapi.components.ProjectComponent;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.startup.StartupManager;
+
+import javax.sound.sampled.*;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class WaifuMotivator implements ProjectComponent {
+
+    private static final String WM_TITLE = "Waifu Motivator";
+
+    private Project project;
+
+    public WaifuMotivator( Project project ) {
+        this.project = project;
+    }
+
+    @Override
+    public void projectOpened() {
+
+        Notifications.Bus.notify( new Notification(
+                WM_TITLE,
+                "Mayushi",
+                "Tuturuuu...Mayushii Desu!",
+                NotificationType.INFORMATION
+        ), project );
+
+        InputStream soundStream = getClass().getClassLoader().getResourceAsStream( "sound/Tuturuu...Mayushii Desu!.wav" );
+        try {
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream( soundStream );
+            Clip clip = AudioSystem.getClip();
+            clip.open( audioInputStream );
+
+            if ( !project.isInitialized() ) {
+                StartupManager startupManager = StartupManager.getInstance( project );
+                startupManager.registerPostStartupActivity( clip::start );
+            } else {
+                clip.start();
+            }
+
+            clip.addLineListener( event -> {
+                if ( event.getType() == LineEvent.Type.STOP ) {
+                    clip.close();
+                }
+            } );
+        } catch ( IOException | LineUnavailableException | UnsupportedAudioFileException e ) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
