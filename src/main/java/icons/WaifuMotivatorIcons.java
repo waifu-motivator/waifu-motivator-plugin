06dd5fff481e5c910fcdc646f5262bdc6de6885d
Zaerald Denze Lungos <zaerald.zd@gmail.com>
2020-05-16 18:31:01 +0800

    feat: revamp toolbar menu (#105)

---
+++ src/main/java/icons/WaifuMotivatorIcons.java
@@ -0,0 +1,11 @@
+package icons;
+
+import com.intellij.openapi.util.IconLoader;
+
+import javax.swing.*;
+
+public interface WaifuMotivatorIcons {
+
+    Icon MENU = IconLoader.getIcon( "/icons/wmp.svg" );
+
+}
