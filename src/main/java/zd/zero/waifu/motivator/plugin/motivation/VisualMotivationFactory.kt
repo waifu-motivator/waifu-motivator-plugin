5bdd1b6a90216ea17b3cb6ebfb7b8eeb0e769970
Alex Simons <alexsimons9999@gmail.com>
2020-08-25 06:13:49 -0500

    Expanded on visual motivate me

--- src/main/java/zd/zero/waifu/motivator/plugin/motivation/VisualMotivationFactory.kt
+++ src/main/java/zd/zero/waifu/motivator/plugin/motivation/VisualMotivationFactory.kt
@@ -3,6 +3,7 @@ package zd.zero.waifu.motivator.plugin.motivation
 import com.intellij.openapi.project.Project
 import zd.zero.waifu.motivator.plugin.alert.VisualWaifuNotification
 import zd.zero.waifu.motivator.plugin.alert.AlertConfiguration
+import zd.zero.waifu.motivator.plugin.alert.NonTitledVisualNotification
 import zd.zero.waifu.motivator.plugin.assets.MotivationAsset
 import zd.zero.waifu.motivator.plugin.player.WaifuSoundPlayerFactory
 
@@ -14,6 +15,17 @@ object VisualMotivationFactory : WaifuMotivationFactory {
     ): WaifuMotivation =
         VisualMotivation(
             VisualWaifuNotification(motivation, project),
+            WaifuSoundPlayerFactory.createPlayer(motivation.soundFilePath),
+            config
+        )
+
+    fun constructNonTitledMotivation(
+        project: Project,
+        motivation: MotivationAsset,
+        config: AlertConfiguration
+    ): WaifuMotivation =
+        VisualMotivation(
+            NonTitledVisualNotification(motivation, project),
             WaifuSoundPlayerFactory.createPlayer(motivation.soundFilePath),
             config
         )
