da34a6d6b7040b799248213b3c2d61dc7ced6f93
DATBOI <alexsimons9999@gmail.com>
2020-09-14 04:39:36 -0500

    Attempting to listen to install event

--- src/main/java/zd/zero/waifu/motivator/plugin/listeners/PluginInstallListener.kt
+++ src/main/java/zd/zero/waifu/motivator/plugin/listeners/PluginInstallListener.kt
@@ -1,2 +1,30 @@
 package zd.zero.waifu.motivator.plugin.listeners
 
+import com.intellij.ide.plugins.DynamicPluginListener
+import com.intellij.ide.plugins.IdeaPluginDescriptor
+import zd.zero.waifu.motivator.plugin.WaifuMotivator
+import zd.zero.waifu.motivator.plugin.onboarding.UpdateNotification
+
+class PluginInstallListener : DynamicPluginListener {
+    override fun beforePluginLoaded(pluginDescriptor: IdeaPluginDescriptor) {
+    }
+
+    override fun beforePluginUnload(pluginDescriptor: IdeaPluginDescriptor, isUpdate: Boolean) {
+    }
+
+    override fun checkUnloadPlugin(pluginDescriptor: IdeaPluginDescriptor) {
+    }
+
+    override fun pluginLoaded(pluginDescriptor: IdeaPluginDescriptor) {
+        if (pluginDescriptor.pluginId.idString == WaifuMotivator.PLUGIN_ID) {
+            UpdateNotification.sendMessage(
+                "Ravioli Ravioli",
+                "Give me the formuoli"
+            )
+        }
+    }
+
+    override fun pluginUnloaded(pluginDescriptor: IdeaPluginDescriptor, isUpdate: Boolean) {
+
+    }
+}
