ba97a3a63fdeaa155a2b176b12e4f468703da3f9
DATBOI <alexsimons9999@gmail.com>
2020-09-14 05:19:00 -0500

    Can do stuff on app listener init?

--- src/main/java/zd/zero/waifu/motivator/plugin/listeners/PluginInstallListener.kt
+++ src/main/java/zd/zero/waifu/motivator/plugin/listeners/PluginInstallListener.kt
@@ -6,6 +6,14 @@ import zd.zero.waifu.motivator.plugin.WaifuMotivator
 import zd.zero.waifu.motivator.plugin.onboarding.UpdateNotification
 
 class PluginInstallListener : DynamicPluginListener {
+
+    init {
+        UpdateNotification.sendMessage(
+            "Ravioli Ravioli",
+            "Give me the formuoli"
+        )
+    }
+
     override fun beforePluginLoaded(pluginDescriptor: IdeaPluginDescriptor) {
     }
 
@@ -18,7 +26,7 @@ class PluginInstallListener : DynamicPluginListener {
     override fun pluginLoaded(pluginDescriptor: IdeaPluginDescriptor) {
         if (pluginDescriptor.pluginId.idString == WaifuMotivator.PLUGIN_ID) {
             UpdateNotification.sendMessage(
-                "I threw it on the ground",
+                "threw it on the ground",
                 "What you think I'm stupid?"
             )
         }
@@ -26,10 +34,6 @@ class PluginInstallListener : DynamicPluginListener {
 
     override fun pluginUnloaded(pluginDescriptor: IdeaPluginDescriptor, isUpdate: Boolean) {
         if (pluginDescriptor.pluginId.idString == WaifuMotivator.PLUGIN_ID) {
-            UpdateNotification.sendMessage(
-                "Ravioli Ravioli",
-                "Give me the formuoli"
-            )
         }
     }
 }
