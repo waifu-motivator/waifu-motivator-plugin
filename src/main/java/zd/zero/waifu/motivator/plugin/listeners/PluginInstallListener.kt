9850d97d3c9f8543406eb6609313ce748786180c
DATBOI <alexsimons9999@gmail.com>
2020-09-14 05:23:55 -0500

    Including project

--- src/main/java/zd/zero/waifu/motivator/plugin/listeners/PluginInstallListener.kt
+++ src/main/java/zd/zero/waifu/motivator/plugin/listeners/PluginInstallListener.kt
@@ -2,16 +2,19 @@ package zd.zero.waifu.motivator.plugin.listeners
 
 import com.intellij.ide.plugins.DynamicPluginListener
 import com.intellij.ide.plugins.IdeaPluginDescriptor
+import com.intellij.openapi.project.ProjectManager
 import zd.zero.waifu.motivator.plugin.WaifuMotivator
 import zd.zero.waifu.motivator.plugin.onboarding.UpdateNotification
 
 class PluginInstallListener : DynamicPluginListener {
 
     init {
-        UpdateNotification.sendMessage(
-            "Ravioli Ravioli",
-            "Give me the formuoli"
-        )
+        ProjectManager.getInstance().openProjects.forEach {
+            UpdateNotification.sendMessage(
+                "threw it on the ground",
+                "What you think I'm stupid?"
+            )
+        }
     }
 
     override fun beforePluginLoaded(pluginDescriptor: IdeaPluginDescriptor) {
@@ -25,10 +28,14 @@ class PluginInstallListener : DynamicPluginListener {
 
     override fun pluginLoaded(pluginDescriptor: IdeaPluginDescriptor) {
         if (pluginDescriptor.pluginId.idString == WaifuMotivator.PLUGIN_ID) {
-            UpdateNotification.sendMessage(
-                "threw it on the ground",
-                "What you think I'm stupid?"
-            )
+            ProjectManager.getInstance().openProjects.forEach {
+                UpdateNotification.sendMessage(
+                    "Ravioli Ravioli",
+                    "Give me the formuoli",
+                    it
+                )
+
+            }
         }
     }
 
