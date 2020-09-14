da835b8f3a888a2c0d5c9e6e3f633cdd10d07a61
DATBOI <alexsimons9999@gmail.com>
2020-09-14 06:14:30 -0500

    ಠ_ಠ

--- src/main/java/zd/zero/waifu/motivator/plugin/listeners/PluginInstallListener.kt
+++ src/main/java/zd/zero/waifu/motivator/plugin/listeners/PluginInstallListener.kt
@@ -2,12 +2,8 @@ package zd.zero.waifu.motivator.plugin.listeners
 
 import com.intellij.ide.plugins.DynamicPluginListener
 import com.intellij.ide.plugins.IdeaPluginDescriptor
-import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManager
-import com.intellij.openapi.project.ProjectManagerListener
 import zd.zero.waifu.motivator.plugin.WaifuMotivator
-import zd.zero.waifu.motivator.plugin.onboarding.UpdateNotification
-
 
 class PluginInstallListener : DynamicPluginListener {
 
